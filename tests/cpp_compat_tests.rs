0fb5a05925dd1b7a10cc2c621531377a4df4df93
Weixing-Zhang <weixing.zhang365@gmail.com>
2025-05-04 21:55:19 -0700

    Addressed suggestions

--- tests/cpp_compat_tests.rs
+++ tests/cpp_compat_tests.rs
@@ -1,33 +1,33 @@
-#![cfg(feature = "rust")]
+#![cfg(all(feature = "rust", feature = "cpp"))]
 
-use fastpfor::cpp::{Codec32, FastPFor128Codec};
-use fastpfor::rust::{FastPFOR, Integer, BLOCK_SIZE_128, DEFAULT_PAGE_SIZE};
+use fastpfor::cpp::Codec32;
+use fastpfor::rust::Integer;
+use fastpfor::{cpp, rust};
 use std::io::Cursor;
 
 #[test]
 fn test_rust_decompresses_cpp_encoded_data() {
-    let test_input_size = 256;
-    let input: Vec<u32> = (0..test_input_size).map(|i| (i * 32) ^ (i >> 1)).collect();
+    let input: Vec<u32> = (0..256).map(|i| (i * 32) ^ (i >> 1)).collect();
 
     // Buffer for the C++ encoded
     let mut compressed_buffer = vec![0; input.len()];
 
     // C++ encoding
-    let codec_cpp = FastPFor128Codec::new();
+    let codec_cpp = cpp::FastPFor128Codec::new();
     let encoded_cpp = codec_cpp
         .encode32(&input, &mut compressed_buffer)
-        .expect("C++ compression failed");
+        .unwrap();
     let compressed_len = encoded_cpp.len();
 
     // C++ decoding
     let mut decoded_by_cpp = vec![0; input.len()];
     let decoded_cpp = codec_cpp
         .decode32(encoded_cpp, &mut decoded_by_cpp)
-        .expect("C++ decompression failed");
+        .unwrap();
 
     // Rust decoding
     let mut input_offset = Cursor::new(0u32);
-    let mut codec_rs = FastPFOR::new(DEFAULT_PAGE_SIZE, BLOCK_SIZE_128);
+    let mut codec_rs = rust::FastPFOR::new(rust::DEFAULT_PAGE_SIZE, rust::BLOCK_SIZE_128);
     let mut decoded_by_rust = vec![0; input.len()];
     codec_rs
         .uncompress(
@@ -37,7 +37,7 @@ fn test_rust_decompresses_cpp_encoded_data() {
             &mut decoded_by_rust,
             &mut Cursor::new(0u32),
         )
-        .expect("Rust decompression failed");
+        .unwrap();
 
     assert_eq!(
         decoded_cpp.len(),
