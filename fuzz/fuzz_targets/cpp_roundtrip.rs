8fd5a9914d3dacb055381104dc810bad69fa12e7
Frank Elsinga <frank@elsinga.de>
2026-02-14 16:29:58 +0100

    fix clippy

--- fuzz/fuzz_targets/cpp_roundtrip.rs
+++ fuzz/fuzz_targets/cpp_roundtrip.rs
@@ -17,7 +17,7 @@ fuzz_target!(|data: FuzzInput| {
 
     // Now decompress
     let mut decoded = vec![0u32; input.len() * 2 + 1024];
-    let dec_slice = codec.decode32(&enc_slice, &mut decoded).unwrap();
+    let dec_slice = codec.decode32(enc_slice, &mut decoded).unwrap();
 
     // Verify roundtrip
     if dec_slice.len() + input.len() < 200 {
@@ -51,81 +51,81 @@ impl std::fmt::Debug for FuzzInput {
 
 #[derive(Clone, Copy, Eq, PartialEq, arbitrary::Arbitrary, Debug)]
 enum FuzzCodec {
-    BP32Codec,
-    CopyCodec,
-    FastBinaryPacking8Codec,
-    FastPFor128Codec,
-    FastPFor256Codec,
-    FastBinaryPacking16Codec,
-    FastBinaryPacking32Codec,
-    MaskedVByteCodec,
-    NewPForCodec,
-    OptPForCodec,
-    PFor2008Codec,
-    PForCodec,
-    SimdBinaryPackingCodec,
-    SimdFastPFor128Codec,
-    SimdFastPFor256Codec,
-    SimdGroupSimpleCodec,
-    SimdGroupSimpleRingBufCodec,
-    SimdNewPForCodec,
-    SimdOptPForCodec,
-    SimdPForCodec,
-    SimdSimplePForCodec,
-    // Simple16Codec, // cannot encode arbitrary bytes
-    // Simple8bCodec, // cannot encode arbitrary bytes
-    // Simple8bRleCodec, // cannot encode arbitrary bytes
-    // Simple9Codec, // cannot encode arbitrary bytes
-    // Simple9RleCodec, // cannot encode arbitrary bytes
-    // SimplePForCodec, // cannot encode arbitrary bytes
-    // SnappyCodec,  // Conditional with #ifdef
-    StreamVByteCodec,
-    VByteCodec,
-    VarIntCodec,
-    // VarIntG8iuCodec,  // Conditional with #ifdef
-    VarIntGbCodec,
-    // VsEncodingCodec,  // This is leaking memory
+    BP32,
+    Copy,
+    FastBinaryPacking8,
+    FastPFor128,
+    FastPFor256,
+    FastBinaryPacking16,
+    FastBinaryPacking32,
+    MaskedVByte,
+    NewPFor,
+    OptPFor,
+    PFor2008,
+    PFor,
+    SimdBinaryPacking,
+    SimdFastPFor128,
+    SimdFastPFor256,
+    SimdGroupSimple,
+    SimdGroupSimpleRingBuf,
+    SimdNewPFor,
+    SimdOptPFor,
+    SimdPFor,
+    SimdSimplePFor,
+    // Simple16, // cannot encode arbitrary bytes
+    // Simple8b, // cannot encode arbitrary bytes
+    // Simple8bRle, // cannot encode arbitrary bytes
+    // Simple9, // cannot encode arbitrary bytes
+    // Simple9Rle, // cannot encode arbitrary bytes
+    // SimplePFor, // cannot encode arbitrary bytes
+    // Snappy,  // Conditional with #ifdef
+    StreamVByte,
+    VByte,
+    VarInt,
+    // VarIntG8iu,  // Conditional with #ifdef
+    VarIntGb,
+    // VsEncoding,  // This is leaking memory
 }
 
 impl From<FuzzCodec> for BoxedCodec {
     fn from(codec: FuzzCodec) -> Self {
         match codec {
-            FuzzCodec::BP32Codec => Box::new(BP32Codec::default()),
-            FuzzCodec::CopyCodec => Box::new(CopyCodec::default()),
-            FuzzCodec::FastBinaryPacking8Codec => Box::new(FastBinaryPacking8Codec::default()),
-            FuzzCodec::FastPFor128Codec => Box::new(FastPFor128Codec::default()),
-            FuzzCodec::FastPFor256Codec => Box::new(FastPFor256Codec::default()),
-            FuzzCodec::FastBinaryPacking16Codec => Box::new(FastBinaryPacking16Codec::default()),
-            FuzzCodec::FastBinaryPacking32Codec => Box::new(FastBinaryPacking32Codec::default()),
-            FuzzCodec::MaskedVByteCodec => Box::new(MaskedVByteCodec::default()),
-            FuzzCodec::NewPForCodec => Box::new(NewPForCodec::default()),
-            FuzzCodec::OptPForCodec => Box::new(OptPForCodec::default()),
-            FuzzCodec::PFor2008Codec => Box::new(PFor2008Codec::default()),
-            FuzzCodec::PForCodec => Box::new(PForCodec::default()),
-            FuzzCodec::SimdBinaryPackingCodec => Box::new(SimdBinaryPackingCodec::default()),
-            FuzzCodec::SimdFastPFor128Codec => Box::new(SimdFastPFor128Codec::default()),
-            FuzzCodec::SimdFastPFor256Codec => Box::new(SimdFastPFor256Codec::default()),
-            FuzzCodec::SimdGroupSimpleCodec => Box::new(SimdGroupSimpleCodec::default()),
-            FuzzCodec::SimdGroupSimpleRingBufCodec => {
+            FuzzCodec::BP32 => Box::new(BP32Codec::default()),
+            FuzzCodec::Copy => Box::new(CopyCodec::default()),
+            FuzzCodec::FastBinaryPacking8 => Box::new(FastBinaryPacking8Codec::default()),
+            FuzzCodec::FastPFor128 => Box::new(FastPFor128Codec::default()),
+            FuzzCodec::FastPFor256 => Box::new(FastPFor256Codec::default()),
+            FuzzCodec::FastBinaryPacking16 => Box::new(FastBinaryPacking16Codec::default()),
+            FuzzCodec::FastBinaryPacking32 => Box::new(FastBinaryPacking32Codec::default()),
+            FuzzCodec::MaskedVByte => Box::new(MaskedVByteCodec::default()),
+            FuzzCodec::NewPFor => Box::new(NewPForCodec::default()),
+            FuzzCodec::OptPFor => Box::new(OptPForCodec::default()),
+            FuzzCodec::PFor2008 => Box::new(PFor2008Codec::default()),
+            FuzzCodec::PFor => Box::new(PForCodec::default()),
+            FuzzCodec::SimdBinaryPacking => Box::new(SimdBinaryPackingCodec::default()),
+            FuzzCodec::SimdFastPFor128 => Box::new(SimdFastPFor128Codec::default()),
+            FuzzCodec::SimdFastPFor256 => Box::new(SimdFastPFor256Codec::default()),
+            FuzzCodec::SimdGroupSimple => Box::new(SimdGroupSimpleCodec::default()),
+            FuzzCodec::SimdGroupSimpleRingBuf => {
                 Box::new(SimdGroupSimpleRingBufCodec::default())
             }
-            FuzzCodec::SimdNewPForCodec => Box::new(SimdNewPForCodec::default()),
-            FuzzCodec::SimdOptPForCodec => Box::new(SimdOptPForCodec::default()),
-            FuzzCodec::SimdPForCodec => Box::new(SimdPForCodec::default()),
-            FuzzCodec::SimdSimplePForCodec => Box::new(SimdSimplePForCodec::default()),
-            // FuzzCodec::Simple16Codec => Box::new(Simple16Codec::default()),
-            // FuzzCodec::Simple8bCodec => Box::new(Simple8bCodec::default()),
-            // FuzzCodec::Simple8bRleCodec => Box::new(Simple8bRleCodec::default()),
-            // FuzzCodec::Simple9Codec => Box::new(Simple9Codec::default()),
-            // FuzzCodec::Simple9RleCodec => Box::new(Simple9RleCodec::default()),
-            // FuzzCodec::SimplePForCodec => Box::new(SimplePForCodec::default()),
-            // FuzzCodec::SnappyCodec => Box::new(SnappyCodec::default()),
-            FuzzCodec::StreamVByteCodec => Box::new(StreamVByteCodec::default()),
-            FuzzCodec::VByteCodec => Box::new(VByteCodec::default()),
-            FuzzCodec::VarIntCodec => Box::new(VarIntCodec::default()),
-            // FuzzCodec::VarIntG8iuCodec => Box::new(VarIntG8iuCodec::default()),
-            FuzzCodec::VarIntGbCodec => Box::new(VarIntGbCodec::default()),
-            // FuzzCodec::VsEncodingCodec => Box::new(VsEncodingCodec::default()),
+            FuzzCodec::SimdNewPFor => Box::new(SimdNewPForCodec::default()),
+            FuzzCodec::SimdOptPFor => Box::new(SimdOptPForCodec::default()),
+            FuzzCodec::SimdPFor => Box::new(SimdPForCodec::default()),
+            FuzzCodec::SimdSimplePFor => Box::new(SimdSimplePForCodec::default()),
+            // FuzzCodec::Simple16 => Box::new(Simple16Codec::default()),
+            // FuzzCodec::Simple8b => Box::new(Simple8bCodec::default()),
+            // FuzzCodec::Simple8bRle => Box::new(Simple8bRleCodec::default()),
+            // FuzzCodec::Simple9 => Box::new(Simple9Codec::default()),
+            // FuzzCodec::Simple9Rle => Box::new(Simple9RleCodec::default()),
+            // FuzzCodec::SimplePFor => Box::new(SimplePForCodec::default()),
+            // FuzzCodec::Snappy => Box::new(SnappyCodec::default()),
+            FuzzCodec::StreamVByte => Box::new(StreamVByteCodec::default()),
+            FuzzCodec::VByte => Box::new(VByteCodec::default()),
+            FuzzCodec::VarInt => Box::new(VarIntCodec::default()),
+            // FuzzCodec::VarIntG8iu => Box::new(VarIntG8iuCodec::default()),
+            FuzzCodec::VarIntGb => Box::new(VarIntGbCodec::default()),
+            // FuzzCodec::VsEncoding => Box::new(VsEncodingCodec::default()),
         }
     }
 }
