0b7f0a9515fd66cca4514bbfdba64252e80e50be
Frank Elsinga <frank@elsinga.de>
2026-02-14 16:22:15 +0100

    make fuzzing more actionable

--- fuzz/fuzz_targets/rust_decompress_oracle.rs
+++ fuzz/fuzz_targets/rust_decompress_oracle.rs
@@ -1,9 +1,7 @@
 #![no_main]
 
-use std::num::NonZeroU32;
-
-use fastpfor::cpp::{FastPFor128Codec, FastPFor256Codec};
-use fastpfor::rust::{Codec, FastPFOR, BLOCK_SIZE_128, BLOCK_SIZE_256, DEFAULT_PAGE_SIZE};
+use fastpfor::cpp;
+use fastpfor::rust;
 use fastpfor::CodecToSlice;
 use libfuzzer_sys::fuzz_target;
 
@@ -15,38 +13,58 @@ fuzz_target!(|data: FuzzInput| {
         return;
     }
 
-    // TODO: To make the decder not crash ->  Skip inputs smaller than block size
-    let block_size = NonZeroU32::from(data.codec);
-    let bs = block_size.get() as usize;
-    if input.len() < bs {
+    // TODO: Behaviour differs
+    if data.codec == FuzzCodec::VariableByte {
         return;
     }
 
-    // TODO: To make the decder not crash -> Truncate to block size multiple
-    let last_block_size_multiple = input.len() / bs * bs;
+    // TODO: To make the decoder not crash ->  Skip inputs smaller than block size
+    let block_size = match data.codec {
+        FuzzCodec::FastPFOR256 => 256,
+        FuzzCodec::FastPFOR128 => 128,
+        FuzzCodec::VariableByte => 1,
+        FuzzCodec::JustCopy => 1,
+    };
+    if input.len() < block_size {
+        return;
+    }
+
+    // TODO: To make the decoder not crash -> Truncate to block size multiple
+    let last_block_size_multiple = input.len() / block_size * block_size;
     let input = &input[..last_block_size_multiple];
 
     // First, compress with C++ implementation to get valid compressed data
     let mut cpp_compressed = vec![0u32; input.len() * 2 + 1024];
     let compressed_data = match data.codec {
         FuzzCodec::FastPFOR256 => {
-            let mut cpp_codec = FastPFor256Codec::new();
+            let mut cpp_codec = cpp::FastPFor256Codec::new();
             cpp_codec
                 .compress_to_slice(input, &mut cpp_compressed)
                 .expect("C++ compression failed")
         }
         FuzzCodec::FastPFOR128 => {
-            let mut cpp_codec = FastPFor128Codec::new();
+            let mut cpp_codec = cpp::FastPFor128Codec::new();
             cpp_codec
                 .compress_to_slice(input, &mut cpp_compressed)
                 .expect("C++ compression failed")
+        }
+        FuzzCodec::VariableByte => {
+            let mut cpp_codec = cpp::VByteCodec::new();
+            cpp_codec
+                .compress_to_slice(input, &mut cpp_compressed)
+                .expect("C++ compression failed")
+        }
+        FuzzCodec::JustCopy => {
+            let mut cpp_codec = cpp::CopyCodec::new();
+            cpp_codec
+                .compress_to_slice(input, &mut cpp_compressed)
+                .expect("Rust compression failed")
         }
     };
 
     // Now decompress with rust
     let mut rust_decompressed = vec![0u32; input.len()];
-    let fastpfor = FastPFOR::new(DEFAULT_PAGE_SIZE, block_size);
-    let mut rust_codec = Codec::from(fastpfor);
+    let mut rust_codec = rust::Codec::from(data.codec);
     let rust_result = rust_codec
         .decompress_to_slice(compressed_data, &mut rust_decompressed)
         .expect("Rust decompression failed");
@@ -57,7 +75,7 @@ fuzz_target!(|data: FuzzInput| {
         input.len(),
         "Decompressed length mismatch: Rust={}, C++={}",
         rust_result.len(),
-        cpp_result.len()
+        input.len()
     );
 
     for (i, (&rust_val, &cpp_val)) in rust_result.iter().zip(input.iter()).enumerate() {
@@ -79,13 +97,23 @@ struct FuzzInput {
 enum FuzzCodec {
     FastPFOR256,
     FastPFOR128,
+    VariableByte,
+    JustCopy,
 }
 
-impl From<FuzzCodec> for NonZeroU32 {
+impl From<FuzzCodec> for rust::Codec {
     fn from(codec: FuzzCodec) -> Self {
         match codec {
-            FuzzCodec::FastPFOR256 => BLOCK_SIZE_256,
-            FuzzCodec::FastPFOR128 => BLOCK_SIZE_128,
+            FuzzCodec::FastPFOR256 => rust::Codec::from(rust::FastPFOR::new(
+                rust::DEFAULT_PAGE_SIZE,
+                rust::BLOCK_SIZE_256,
+            )),
+            FuzzCodec::FastPFOR128 => rust::Codec::from(rust::FastPFOR::new(
+                rust::DEFAULT_PAGE_SIZE,
+                rust::BLOCK_SIZE_128,
+            )),
+            FuzzCodec::VariableByte => rust::Codec::from(rust::VariableByte::new()),
+            FuzzCodec::JustCopy => rust::Codec::from(rust::JustCopy::new()),
         }
     }
 }
